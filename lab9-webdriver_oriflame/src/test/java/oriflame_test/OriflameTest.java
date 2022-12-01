acbbe06b31263374b61aafba0ceda85c21778c13
Polly Laptop <p.logash@gmail.com>
2022-12-01 04:55:02 +0300

    feat: add AddToCartTest

--- lab9-webdriver_oriflame/src/test/java/oriflame_test/OriflameTest.java
+++ lab9-webdriver_oriflame/src/test/java/oriflame_test/OriflameTest.java
@@ -1,6 +1,7 @@
 package oriflame_test;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -32,6 +33,25 @@ public class OriflameTest {
         WebElement productCode = webDriver.findElement(By.xpath("//div[@class='color-code']//span[contains(@class,'code')]"));
         Assert.assertEquals(productCode.getText(), EXPECTED_CODE);
     }
+
+    
+    @Test
+    public void testAddItemToCart() {
+        webDriver.get("https://by.oriflame.com/products/product?code=37769");
+        String EXPECTED_AMOUNT = "1";
+
+        WebElement buttonAddToCart = webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(@class,'k-button add-to-basket be-by')]")));
+        buttonAddToCart.click();
+
+        WebElement cartIcon = webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@class='MuiTypography-root MuiTypography-body1 MuiLink-root MuiLink-underlineAlways top-area-4v5mp']")));
+        cartIcon.click();
+
+        WebElement numberOfProduct = webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@data-testid='Checkout-shopping-bag-step-1-products-item-37769-P-quantity']")));
+
+        Assert.assertEquals(numberOfProduct.getAttribute("value"), EXPECTED_AMOUNT);
+    }
+
+
 
     @AfterMethod(alwaysRun = true)
     public void browserTearDown()  {
